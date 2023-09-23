1e6da70cbc4fbd4084a8552156d2ade14cedf7d4
Alex <al3xxxgm@gmail.com>
2023-09-24 01:39:21 +0300

    added maven local

--- dotstablayout/build.gradle.kts
+++ dotstablayout/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    `maven-publish`
 }
 
 android {
@@ -9,6 +10,7 @@ android {
 
     defaultConfig {
         minSdk = 24
+        version = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -37,7 +39,18 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("ReleaseArr") {
+                groupId = "host.capitalquiz"
+                artifactId = "dotstablayout"
+                version = "${project.version}"
+
+                from(components["release"])
+            }
+        }
+    }
 }
